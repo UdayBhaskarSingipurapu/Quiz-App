c310a87d30fc3632fb916cc4a6ede7381a8da3e9
Uday Bhaskar Singipurapu <s.udaybhaskar2005@gamil.com>
2025-11-15 21:04:48 +0530

    Secure Env vars

--- Question-Service/src/main/java/com/microservices_quiz/Question/Service/QuestionServiceApplication.java
+++ Question-Service/src/main/java/com/microservices_quiz/Question/Service/QuestionServiceApplication.java
@@ -1,10 +1,24 @@
 package com.microservices_quiz.Question.Service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuestionServiceApplication {
+
+    static {
+        // Load .env file
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        // Load every entry into System properties for Spring Boot
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
 
 	public static void main(String[] args) {
         SpringApplication.run(QuestionServiceApplication.class, args);
